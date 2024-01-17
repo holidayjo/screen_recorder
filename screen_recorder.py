286ec93623635bb9682a7d4a5b24a5d03c88b8fb
Holiday_Jo <ossam1996@gmail.com>
2024-01-18 08:59:09 +0900

    begin

---
+++ screen_recorder.py
@@ -0,0 +1,15 @@
+# I tried to record my pc screen with some window programmes but it includes water marks.So I decided to make my own.
+# This is from https://www.geeksforgeeks.org/create-a-screen-recorder-using-python/
+
+import pyautogui
+import cv2
+import numpy as np
+
+resolution = (1920, 1080)
+codec      = cv2.VideoWriter_fourcc(*"XVID")
+filename   = "Recodrding.avi"
+fps        = 30.0
+out        = cv2.VideoWriter(filename, codec, fps, resolution)
+
+
+
